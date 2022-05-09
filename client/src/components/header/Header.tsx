bbf7f58841570a14dc60914d25bb6ed404e47446
micand89 <89338483+micand89@users.noreply.github.com>
2022-05-09 09:31:09 +0200

    skapat en header
    skapat en header i client

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import css from './Header.module.css'
+
+function Header () {
+    return (
+        <div className={css.headerContainer}>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header
