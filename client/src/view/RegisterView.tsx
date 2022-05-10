f96c7fe548701ad01fe51814eae3bf581cfde64a
Batalawata <82433408+Batalawata@users.noreply.github.com>
2022-05-10 14:47:18 +0200

    Update RegisterView.tsx

--- client/src/view/RegisterView.tsx
+++ client/src/view/RegisterView.tsx
@@ -1,59 +1,34 @@
-import css from '../view/StartView.module.css'
-import IUser from 
-import {findAllByDisplayValue} from "@testing-library/react";
 import React, { useState } from 'react'
 
 type Props = { 
-  saveTodo: (e: React.FormEvent, formData: IUser | any) => void 
+  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void 
 }
 
 const AddTodo: React.FC<Props> = ({ saveTodo }) => {
-    const [formData, setFormData] = useState<IUser | {}>()
+  const [formData, setFormData] = useState<ITodo | {}>()
 
-    const registerUser = async (req: Request, res: Response): Promise<void> => {
-        try {
-            const body = req.body as unknown as Pick<IUser, 'fullName' | 'eMail' | 'pass'>
-    
-            // eslint-disable-next-line @typescript-eslint/no-use-before-define
-            const User: IUser = new user({
-                fullName: body.fullName,
-                eMail: body.eMail,
-                pass: body.pass
-            }) 
-    
-            const newUser: IUser = await User.save()
-            const allUsers: IUser[] = await User.find()
-    
-            res.body = ({ message: 'User registered!', user: newUser, users: allUsers })
-        } catch (error) {
-            throw error
-        }
-    }
+  const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    })
+  }
 
-    const handleChangedFullName = (e: any) => {
-        setUserFullName(e.target.value)
-    }
-
-    return (
-        <div className={css.mainGridContainer}>
-            <div className={css.item}>
-                <h1 className={css.text}>BookFace</h1>
-                <h3 className={css.textOne}>Create a new account</h3>
-            </div>
-
-            <div className={css.itemOne}>
-                <form>
-                    <input type="text" placeholder='Full Name' onChange={(e) => handleChangedFullName(e) } value ={userFullName}/>
-                    <br/>
-                    <input type="text" placeholder='e-mail' onChange={(e) => handleChangedFullName }/>
-                    <br/>
-                    <input type="text" placeholder='Password' onChange={(e) => handleChangedFullName }/>
-                    <hr/>
-                    <input type={"submit"} className={css.buttonCreateNewAccount}/>Create new account
-                </form>
-            </div>
+  return (
+    <form className='Form' onSubmit={(e) => saveTodo(e, formData)}>
+      <div>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input onChange={handleForm} type='text' id='name' />
         </div>
-    )
+        <div>
+          <label htmlFor='description'>Description</label>
+          <input onChange={handleForm} type='text' id='description' />
+        </div>
+      </div>
+      <button disabled={formData === undefined ? true: false} >Add Todo</button>
+    </form>
+  )
 }
 
-export default RegisterView
+export default AddTodo
