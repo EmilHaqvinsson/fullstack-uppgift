132f2c221604f3fd8f5940a391c9aca596aa9cc7
micand89 <89338483+micand89@users.noreply.github.com>
2022-05-11 15:18:20 +0200

    pysslat med frontend
    nu har vi en smurf sida

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import css from './Header.module.css'
 function Header () {
     return (
         <div className={css.headerContainer}>
-            <h1 className={css.text}>Bookface</h1>
+            <h1 className={css.text}>Welcome to the SmurfBooks</h1>
         </div>
     )
 }
