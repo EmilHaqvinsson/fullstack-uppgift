94826e99d569a89a7566f8a3f9f82cd15171f229
micand89 <89338483+micand89@users.noreply.github.com>
2022-05-13 12:40:04 +0200

    skapat message funktion i backend

---
+++ server/src/routes/MessageRoutes.ts
@@ -0,0 +1,13 @@
+import {Express} from 'express'
+import MessageController from '../controllers/MessageController'
+
+const MessageRoutes = (server: Express) => {
+    server.post('/message', MessageController.registerMessage)
+    // server.get('/message/all', MessageController.getAllMessages)
+    // server.get('/message/:id', MessageController.getMessageById)
+    // server.get('message/name/:name/:eMail', MessageController.getMessageByNameAndEmail)
+    // server.put('/message/:id', MessageController.updateMessageById)
+    // server.delete('/message/:id', MessageController.deleteMessageById)
+}
+
+export default MessageRoutes
