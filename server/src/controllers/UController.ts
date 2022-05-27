713e0385e3499b8705a7b5204a5cfce6cbc1afab
Batalawata <82433408+Batalawata@users.noreply.github.com>
2022-05-27 08:49:41 +0200

    Login funktionell

--- server/src/controllers/UController.ts
+++ server/src/controllers/UController.ts
@@ -4,10 +4,10 @@ import {Request, Response} from "express";
 import {CreateU, ReadU} from "../interface/InterFace";
 import UModel from "../models/UModel";
 import bcrypt from 'bcrypt'
-import { ReadMessage } from "../interface/IMessage";
+// import { ReadMessage } from "../interface/IMessage";
 
 const saltRounds: number = 10
-const encryptedPassword = async (pass: string) => {
+export const encryptedPassword = async (pass: string) => {
     let newPass: string = ''
     await bcrypt.hash(pass,saltRounds).then(function (hash: any){
         newPass = hash;
@@ -15,92 +15,9 @@ const encryptedPassword = async (pass: string) => {
     return newPass
 }
 
-const registerUser = async (req: Request, res: Response) => {
-    try {
-        Logger.info('createUser()')
-        Logger.http(req.body)
-        let {fullName, eMail, pass} = req.body
-        pass = await encryptedPassword(pass)
-        if (fullName && eMail && pass) {
-            const newobject: CreateU = {
-                fullName: fullName,
-                eMail: eMail,
-                pass: pass
-            }
-            Logger.http(newobject)
-
-            const user = new UModel(newobject)
-            const dbResponse = await user.save()
-            Logger.http(dbResponse)
-            res.status(StatusCode.CREATED).send('Användare skapad!')
-        } else {
-            Logger.error('name, fullName or eMail failed')
-            res.status(StatusCode.BAD_REQUEST).send({
-                message: 'Faulty body'
-            })
-        }
-    } catch (error) {
-        Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
-                error: 'Det gick inte att skapa användare'
-            }
-        )
-    }
-}
-
-async function login(req: Request, res: Response) {
-    let message: any
-    let resultOfLogin: object
-    let eMail = req.body.eMail
-    let pass = req.body.pass
-    Logger.info('ENDPOINT REACHED; TRYING LOGIN..')
-    try {
-        if (pass && eMail) {
-            Logger.http('"' + eMail + '" and "' + pass + '" are users email and pass.')
-            const foundUser = await UModel.findOne({eMail})
-            const hashIt = bcrypt.hashSync(JSON.stringify(Date.now()), 10)
-            let isAuth: boolean | string 
-            let authToken: string = hashIt
-            foundUser ? isAuth = bcrypt.compareSync(pass, foundUser.pass) : isAuth = false
-            isAuth ? isAuth = authToken : isAuth = 'Could not log in user ' + foundUser?.eMail + ' with that pass . Please try again.'
-            const whoIsUser = await UModel.findOne({eMail})
-
-            resultOfLogin = {
-                authenticated: isAuth,
-                fullName: whoIsUser?.fullName,
-                email: whoIsUser?.eMail
-            } 
-            // const tryLogin = foundUser ? resultOfLogin = {authenticated: isAuth, message: 'User was authenticated! Welcome.'}
-            //                             : resultOfLogin = {authenticated: isAuth, message: 'No user could be found with that email.'}
-
-            // tryLogin.authenticated ? resultOfLogin = {authenticated: tryLogin.authenticated, message: resultOfLogin}
-            //                         : resultOfLogin = {authenticated: false, message: 'Login didnt work.'}
-
-            Logger.info(resultOfLogin)
-            res.send(resultOfLogin)
-        } else if (!eMail) {
-            res.status(StatusCode.BAD_REQUEST)
-            resultOfLogin = {authenticated: false, message:`You need to provide an email.`}
-            res.send(message)
-        } else if (!pass) {
-            res.status(StatusCode.BAD_REQUEST)
-            message = `You need to provide a password.`
-            res.send(message)
-        } else {
-            message += '\n'
-            res.status(StatusCode.BAD_REQUEST)
-            res.send(res.status + message)
-        }
-    } catch (error) {
-        Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
-            error: 'Det gick inte att hämta användaren efter namn och eMail'
-        })
-    }
-}
-
 function getAllUsers(req: Request, res: Response) {
     try {
+        // @ts-ignore
         UModel.find({}, '', (error: ErrorCallback, users: Array<ReadU>) => {
             if (error) {
                 Logger.error(error);
@@ -123,7 +40,8 @@ function getAllUsers(req: Request, res: Response) {
 
 const getUserById = (req: Request, res: Response) => {
     try {
-        UModel.findById(req.params.id, (error: ErrorCallback, users: Array<ReadU>) => {
+        // @ts-ignore
+        UModel.findById(req.params.id, (error: ErrorCallback, users: ReadU) => {
             if (error) {
                 Logger.error(error)
                 res.status(StatusCode.BAD_REQUEST).send({
@@ -144,6 +62,7 @@ const getUserById = (req: Request, res: Response) => {
 
 const getUserByNameAndEmail = (req: Request, res: Response) => {
     try {
+        // @ts-ignore
         UModel.find({ fullName: req.params.name, eMail: req.params.eMail }, '', (error: ErrorCallback, user: Array<ReadU>) => {
             if (error) {
                 Logger.error(error)
@@ -173,28 +92,30 @@ const updateUserById = (req: Request, res: Response) => {
             pass: req.body.pass
         }
         Logger.debug(updatedUser)
-        UModel.findByIdAndUpdate(req.params.id, updatedUser, (error: ErrorCallback, user: ReadU) => {
+        UModel.findByIdAndUpdate(req.params.id, updatedUser, {new : true }, (error: any , user: any) => {
             if (error) {
                 Logger.error(error)
                 res.status(StatusCode.BAD_REQUEST).send({
                     error: 'Fel vid uppdatering av användare'
                 })
             } else {
-                Logger.http(user)
-                res.status(StatusCode.OK).send(user ? user : {
+                Logger.debug(user)
+                res.status(StatusCode.NOT_FOUND).send(user ? user : {
                     message: `Användare med id '${req.params.id}' hittades inte`
                 })
             }
         })
     } catch (error) {
         Logger.error(error)
-        res.status(StatusCode.BAD_REQUEST).send({
+        res.status(StatusCode.).send({
             error: 'Fel vid uppdatering av användare'
         })
     }
 }
+
 const deleteUserById = (req: Request, res: Response) => {
     try {
+        // @ts-ignore
         UModel.findByIdAndRemove(req.params.id, (error: ErrorCallback, user: ReadU) => {
             if (error) {
                 Logger.error(error)
@@ -204,7 +125,7 @@ const deleteUserById = (req: Request, res: Response) => {
             } else {
                 Logger.http(user)
                 res.status(StatusCode.OK).json(
-                    user ? { message: `Användare med id '${req.params.id}' har tagits bort från databasen!` }
+                    user ? { message: `Användare med id ${req.params.id} har tagits bort från databasen!` }
                         : { message: `Användare med id '${req.params.id}'hittades inte!` })
             }
         })
@@ -217,8 +138,6 @@ const deleteUserById = (req: Request, res: Response) => {
 }
 
 export default {
-    registerUser,
-    login,
     getAllUsers,
     getUserById,
     getUserByNameAndEmail,
