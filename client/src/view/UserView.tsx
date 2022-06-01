7c57ecd42649d1c70b8fa8361bb418dbfa1acb25
Aram <Aram.10@hotmail.com>
2022-06-01 10:46:34 +0200

    commit
    färdig med resp

--- client/src/view/UserView.tsx
+++ client/src/view/UserView.tsx
@@ -1,18 +1,20 @@
 import css from '../view/UserView.module.css'
-import ARAM from '../utils/image/ARAM.jpg'
+import smurffen from '../utils/image/smurffen.jpg'
 
 function UserView() {
     return (
         <>
             <div className={css.section}>
                 <section>
-                    <img className={css.imageUser} src={ARAM} alt="user"/>
+                    <img className={css.imageUser} src={smurffen} alt="user"/>
                     <p>
-                        <input type="text" placeholder="FirstName"/>
-                    </p>
-                    <p>
+                        <input type="text" placeholder="Name"/>
                         <input type="text" placeholder="LastName"/>
+                        <br/>
+                        <input type="text" placeholder="E-mail"/>
+                        <input type="text" placeholder="Work"/>
                     </p>
+
 
                 </section>
             </div>
@@ -21,22 +23,3 @@ function UserView() {
 }
 
 export default UserView
-
-
-{/*                <section className={css.section}>
-                    <div className={css.userDiv}>
-                        <h2>Welcome.. (Lägg in användare som är inloggad?)</h2>
-                        <div className={css.inputDiv}>
-                            <div className={css.myDIV}>
-                                <div className={css.item3}>
-                                    <img className={css.imageUser} src={ARAM} alt="user"/>
-                                    <input type="text" placeholder="FirstName"/>
-                                    <input type="text" placeholder="LastName"/>
-                                    <input type="text"/>
-                                    <input type="text"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>*/
-}
